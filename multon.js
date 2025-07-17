77536c82dede3fbae200f6ab9a682880daf9af06
waversound <syutkinxvslv@yandex.ru>
2025-07-17 22:41:47 +0300

    Update multon.js

--- multon.js
+++ multon.js
@@ -1,28 +1,39 @@
 (() => {
-    const plugin_id = 'plugin_menu_multmovie';
-    const menu_id = 'multmovie';
+    const plugin = {
+        name: 'multmovie_menu',
+        version: '1.0',
+        description: 'Добавляет пункт меню Мультфильмы вместо Аниме',
 
-    if (!Lampa.Menu.get(menu_id)) {
-        Lampa.Menu.append({
-            title: Lampa.Lang.translate('menu_multmovie') || 'Мультфильмы',
-            component: 'category',
-            id: menu_id,
-            position: 3,
-            url: 'movie',
-            source: 'tmdb',
-            filter: {
-                genre: ['мультфильм']
-            }
-        });
+        init() {
+            // Переопределяем меню
+            Lampa.Listener.follow('menu', (event) => {
+                if(event.action === 'ready') {
+                    // Удаляем пункт Аниме из меню
+                    event.instance.menu_items = event.instance.menu_items.filter(item => item.action !== 'anime');
 
-        console.log(`[${plugin_id}] Пункт "Мультфильмы" добавлен через локализацию`);
-    }
+                    // Добавляем пункт Мультфильмы
+                    event.instance.menu_items.push({
+                        title: Lampa.Lang.translate('menu_multmovie'),
+                        action: 'multmovie',
+                        icon: 'multmovie' // Можно заменить на иконку
+                    });
 
-    if (window.Lampa?.Plugins) {
-        Lampa.Plugins[plugin_id] = {
-            name: 'Меню "Мультфильмы" (через локализацию)',
-            version: '1.0',
-            author: 'ChatGPT'
-        };
-    }
+                    // Обновляем отображение меню
+                    event.instance.render();
+                }
+
+                // Обработка клика по новому пункту
+                if(event.action === 'select' && event.item.action === 'multmovie') {
+                    // Открываем подборку мультфильмов (здесь пример с категориями)
+                    Lampa.Listener.trigger('content', {
+                        type: 'collection',
+                        collection: 'multmovie'
+                    });
+                    event.instance.close();
+                }
+            });
+        }
+    };
+
+    Lampa.Plugins.add(plugin);
 })();
