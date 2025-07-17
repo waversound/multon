ae6e1e7e974dc9d69fb067d15830fd46b2a9b6ff
waversound <syutkinxvslv@yandex.ru>
2025-07-17 23:22:47 +0300

    Update multon.js

--- multon.js
+++ multon.js
@@ -1,53 +1,62 @@
 "use strict";
+(function() {
+  var iconInactive = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M5.5 3a3.5 3.5 0 0 1 3.25 4.8a7.017 7.017 0 0 0 -2.424 2.1a3.5 3.5 0 1 1 -.826 -6.9z"/>
+      <path d="M18.5 3a3.5 3.5 0 1 1 -.826 6.902a7.013 7.013 0 0 0 -2.424 -2.103a3.5 3.5 0 0 1 3.25 -4.799z"/>
+      <path d="M12 14m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"/>
+    </svg>`;
 
-(function () {
-    function init() {
-        const menuTitle = 'Мультфильмы';
+  var iconActive = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <!-- сюда можешь вставить активную иконку, например с увеличенной толщиной stroke -->
+      <path d="M5.5 3a3.5 3.5 0 0 1 3.25 4.8a7.017 7.017 0 0 0 -2.424 2.1a3.5 3.5 0 1 1 -.826 -6.9z"/>
+      <path d="M18.5 3a3.5 3.5 0 1 1 -.826 6.902a7.013 7.013 0 0 0 -2.424 -2.103a3.5 3.5 0 0 1 3.25 -4.799z"/>
+      <path d="M12 14m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"/>
+    </svg>`;
 
-        const svgIcon = `
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 width="100%" height="100%" viewBox="0 0 24 24"
-                 fill="currentColor"
-                 class="icon icon-tabler icon-tabler-mickey">
-                <path d="M0 0h24v24H0z" fill="none"/>
-                <path d="M5.5 3a3.5 3.5 0 0 1 3.25 4.8a7.017 7.017 0 0 0 -2.424 2.1a3.5 3.5 0 1 1 -.826 -6.9z" />
-                <path d="M18.5 3a3.5 3.5 0 1 1 -.826 6.902a7.013 7.013 0 0 0 -2.424 -2.103a3.5 3.5 0 0 1 3.25 -4.799z" />
-                <path d="M12 14m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-            </svg>`;
+  var a = function() {
+    var b = "Мультфильмы";
+    var c = ["tmdb", "cub"];
+    var d = "[data-action=\"tv\"]";
+    
+    var f = $(`
+      <li class="menu__item selector" data-action="mad_mult">
+        <div class="menu__ico">${iconInactive}</div>
+        <div class="menu__text">${b}</div>
+      </li>
+    `);
 
-        const menuItem = $(`
-            <li class="menu__item selector" data-action="mad_mult">
-                <div class="menu__ico icon-mult-custom">${svgIcon}</div>
-                <div class="menu__text">${menuTitle}</div>
-            </li>
-        `);
+    // При активации пункта меню меняем иконку
+    f.on("hover:focus", function() {
+      $(this).find(".menu__ico").html(iconActive);
+    });
 
-        menuItem.on("hover:enter", function () {
-            Lampa.Activity.push({
-                url: 'movie',
-                title: `${menuTitle}`,
-                component: 'category',
-                genres: 16,
-                id: 16,
-                source: 'tmdb',
-                card_type: true,
-                page: 1
-            });
-        });
+    // При потере фокуса возвращаем неактивную иконку
+    f.on("hover:blur", function() {
+      $(this).find(".menu__ico").html(iconInactive);
+    });
 
-        // Добавляем пункт после "ТВ"
-        const menu = Lampa.Menu.render();
-        menu.find('[data-action="tv"]').after(menuItem);
+    f.on("hover:enter", function() {
+      var a = Lampa.Activity.active(),
+          e = a.source,
+          source = c.includes(e) ? e : c[0];
+      Lampa.Activity.push({
+        url: "movie",
+        title: `${b} - ${source.toUpperCase()}`,
+        component: "category",
+        genres: 16,
+        id: 16,
+        source: source,
+        card_type: true,
+        page: 1
+      });
+    });
 
-        // Удаляем "Аниме"
-        menu.find('[data-action="anime"]').remove();
-    }
+    Lampa.Menu.render().find(d).after(f);
+  };
 
-    if (window.appready) {
-        init();
-    } else {
-        Lampa.Listener.follow("app", function (e) {
-            if (e.type === "ready") init();
-        });
-    }
+  window.appready ? a() : Lampa.Listener.follow("app", function(b) {
+    if(b.type === "ready") a();
+  });
 })();
