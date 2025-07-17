e26c62e47f944d4180f661141d5d2fe8be1e2d6b
waversound <syutkinxvslv@yandex.ru>
2025-07-17 23:26:02 +0300

    Update multon.js

--- multon.js
+++ multon.js
@@ -1,13 +1,34 @@
 "use strict";
 (function() {
-  var iconInactive = `...`; // твоя неактивная иконка SVG
-  var iconActive = `...`;   // твоя активная иконка SVG
+
+  // SVG иконка неактивная (пример)
+  var iconInactive = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
+         stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-mickey">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M5.5 3a3.5 3.5 0 0 1 3.25 4.8a7.017 7.017 0 0 0 -2.424 2.1a3.5 3.5 0 1 1 -.826 -6.9z" />
+      <path d="M18.5 3a3.5 3.5 0 1 1 -.826 6.902a7.013 7.013 0 0 0 -2.424 -2.103a3.5 3.5 0 0 1 3.25 -4.799z" />
+      <path d="M12 14m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+    </svg>`;
+
+  // SVG иконка активная (пример, можно заменить)
+  var iconActive = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
+         stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-mickey">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M5.5 3a3.5 3.5 0 0 1 3.25 4.8a7.017 7.017 0 0 0 -2.424 2.1a3.5 3.5 0 1 1 -.826 -6.9z" />
+      <path d="M18.5 3a3.5 3.5 0 1 1 -.826 6.902a7.013 7.013 0 0 0 -2.424 -2.103a3.5 3.5 0 0 1 3.25 -4.799z" />
+      <path d="M12 14m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+    </svg>`;
 
   var addMultMenuItem = function() {
     var title = "Мультфильмы";
     var sources = ["tmdb", "cub"];
-    var selector = '[data-action="tv"]';
+    var selector = '[data-action="tv"]'; // после Сериалы вставим
 
+    // Создаем пункт меню с нужной иконкой (неактивной)
     var item = $(`
       <li class="menu__item selector" data-action="mad_mult">
         <div class="menu__ico">${iconInactive}</div>
@@ -15,14 +36,17 @@
       </li>
     `);
 
+    // Смена иконки при фокусе (активная)
     item.on("hover:focus", function() {
       $(this).find(".menu__ico").html(iconActive);
     });
 
+    // Возврат неактивной иконки при снятии фокуса
     item.on("hover:blur", function() {
       $(this).find(".menu__ico").html(iconInactive);
     });
 
+    // При выборе пункта меню - переход на категорию "мультфильмы"
     item.on("hover:enter", function() {
       var activity = Lampa.Activity.active();
       var source = sources.includes(activity.source) ? activity.source : sources[0];
@@ -39,10 +63,23 @@
       });
     });
 
-    Lampa.Menu.render().find(selector).after(item);
+    // Вставляем пункт после "Сериалы"
+    var menu = Lampa.Menu.render();
+
+    // Удаляем пункт "Аниме", если есть
+    menu.find('[data-action="anime"]').remove();
+
+    // Вставляем "Мультфильмы"
+    menu.find(selector).after(item);
   };
 
-  window.appready ? addMultMenuItem() : Lampa.Listener.follow("app", function(e) {
-    if (e.type === "ready") addMultMenuItem();
-  });
+  // Запускаем после готовности приложения
+  if (window.appready) {
+    addMultMenuItem();
+  } else {
+    Lampa.Listener.follow("app", function(e) {
+      if (e.type === "ready") addMultMenuItem();
+    });
+  }
+
 })();
