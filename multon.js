d8118050cf9aa372c63c984621b98801899ce475
waversound <syutkinxvslv@yandex.ru>
2025-07-17 23:30:42 +0300

    Update multon.js

--- multon.js
+++ multon.js
@@ -1,80 +1,55 @@
 "use strict";
+(function(){
+  var a = "[data-action=\"mad_mult\"]",
+      b = "\u041C\u0443\u043B\u044C\u0442\u0444\u0438\u043B\u044C\u043C\u044B",
+      c = ["tmdb","cub"],
+      d = "[data-action=\"tv\"]",
+      e = function(a,b){
+        return setTimeout(function(){
+          return $(a).insertAfter($(b))
+        }, 2000)
+      },
+      f = $(`
+        <li class="menu__item selector" data-action="mad_mult">
+          <div class="menu__ico">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.25"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M5.5 3a3.5 3.5 0 0 1 3.25 4.8a7.017 7.017 0 0 0 -2.424 2.1a3.5 3.5 0 1 1 -.826 -6.9z" />
+              <path d="M18.5 3a3.5 3.5 0 1 1 -.826 6.902a7.013 7.013 0 0 0 -2.424 -2.103a3.5 3.5 0 0 1 3.25 -4.799z" />
+              <path d="M12 14m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+            </svg>
+          </div>
+          <div class="menu__text">${b}</div>
+        </li>
+      `);
 
-(function() {
-  var menuSelector = '[data-action="mad_mult"]';
-  var menuText = "Мультфильмы";
-  var sources = ["tmdb", "cub"];
-  var tvSelector = '[data-action="tv"]';
-
-  // Вставка пункта меню после TV
-  function insertMenuItem(menu) {
-    // Создаём элемент пункта меню с твоей SVG-иконкой
-    var menuItem = $(`
-      <li class="menu__item selector" data-action="mad_mult">
-        <div class="menu__ico">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="1"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M5.5 3a3.5 3.5 0 0 1 3.25 4.8a7.017 7.017 0 0 0 -2.424 2.1a3.5 3.5 0 1 1 -.826 -6.9z" />
-            <path d="M18.5 3a3.5 3.5 0 1 1 -.826 6.902a7.013 7.013 0 0 0 -2.424 -2.103a3.5 3.5 0 0 1 3.25 -4.799z" />
-            <path d="M12 14m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-          </svg>
-        </div>
-        <div class="menu__text">${menuText}</div>
-      </li>
-    `);
-
-    // Обработчик выбора пункта меню
-    menuItem.on("hover:enter", function() {
-      var activeActivity = Lampa.Activity.active();
-      var source = activeActivity.source;
-      var usedSource = sources.includes(source) ? source : sources[0];
-
-      Lampa.Activity.push({
-        url: "movie",
-        title: `${menuText} - ${usedSource.toUpperCase()}`,
-        component: "category",
-        genres: 16,
-        id: 16,
-        source: usedSource,
-        card_type: true,
-        page: 1
-      });
+  f.on("hover:enter", function(){
+    var a = Lampa.Activity.active(),
+        d = a.source,
+        e = c.includes(d) ? d : c[0];
+    Lampa.Activity.push({
+      url: "movie",
+      title: `${b} - ${e.toUpperCase()}`,
+      component: "category",
+      genres: 16,
+      id: 16,
+      source: e,
+      card_type: true,
+      page: 1
     });
+  });
 
-    // Вставляем пункт после ТВ
-    Lampa.Menu.render().find(tvSelector).after(menuItem);
-  }
-
-  // Удаление пункта "Аниме"
-  function removeAnime() {
-    $('[data-action="anime"]').remove();
-  }
-
-  // Инициализация плагина после готовности приложения
-  function init() {
-    var menu = Lampa.Menu.render();
-
-    // Удаляем аниме
-    removeAnime();
-
-    // Вставляем мультфильмы
-    insertMenuItem(menu);
-  }
-
-  if (window.appready) {
-    init();
-  } else {
-    Lampa.Listener.follow("app", function(event) {
-      if (event.type === "ready") init();
-    });
-  }
+  Lampa.Menu.render().find(d).after(f);
+  e(a, d);
+  // Убрали вставку аниме
+  // e("[data-action=\"anime\"]", a);
 })();
