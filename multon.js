7bac49f5e4d0a34d8d7ab467ad16b2711f113ab9
waversound <syutkinxvslv@yandex.ru>
2025-07-17 23:17:22 +0300

    Update multon.js

--- multon.js
+++ multon.js
@@ -7,10 +7,9 @@
         const svgIcon = `
             <svg xmlns="http://www.w3.org/2000/svg"
                  width="100%" height="100%" viewBox="0 0 24 24"
-                 fill="none" stroke="currentColor" stroke-width="2"
-                 stroke-linecap="round" stroke-linejoin="round"
+                 fill="currentColor"
                  class="icon icon-tabler icon-tabler-mickey">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M0 0h24v24H0z" fill="none"/>
                 <path d="M5.5 3a3.5 3.5 0 0 1 3.25 4.8a7.017 7.017 0 0 0 -2.424 2.1a3.5 3.5 0 1 1 -.826 -6.9z" />
                 <path d="M18.5 3a3.5 3.5 0 1 1 -.826 6.902a7.013 7.013 0 0 0 -2.424 -2.103a3.5 3.5 0 0 1 3.25 -4.799z" />
                 <path d="M12 14m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
@@ -42,21 +41,6 @@
 
         // Удаляем "Аниме"
         menu.find('[data-action="anime"]').remove();
-
-        // Добавляем CSS для смены цвета при активации
-        const style = `
-            <style>
-                .icon-mult-custom svg {
-                    width: 100%;
-                    height: 100%;
-                    stroke: #999;
-                }
-                .menu__item--active .icon-mult-custom svg {
-                    stroke: #fff;
-                }
-            </style>
-        `;
-        $('head').append(style);
     }
 
     if (window.appready) {
